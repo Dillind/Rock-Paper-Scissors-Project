4967675db0d4c1fda64e045f80c5a02a425c0cd7
Dylan Lindsay <dylan234@tpg.com.au>
2022-08-29 11:19:37 +1000

    added styles.css file

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+// 1. Understand the problem
+// - Create a function called getComputerChoice that will randomly return either 'Rock', 'Paper' or 'Scissors' (RPS).
+// - Write a function that plays a single round of RPS. The function should take two parameters - the playerSelection and computerSelection - and then return a string that delcares the winner of the round like so: "You lose! Paper beats Rock!"
+// - Make your function's playerSelection paramter case-insensitive (so users can input rock, ROCK, rocK etc.)
+// - Write a NEW function called game(). Call the playRound function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
+
+// 2. Plan
+
+// 3. Divide into subproblems
+// -
+
+const getComputerChoice = function () {
+  return;
+};
